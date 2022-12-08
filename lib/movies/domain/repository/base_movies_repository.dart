276ced36fe6864129b9c79f5567b201cc71d16ec
shimaa2000 <shimaam5256@gmail.com>
2022-12-08 20:43:50 +0200

    movie details not contained

--- lib/movies/domain/repository/base_movies_repository.dart
+++ lib/movies/domain/repository/base_movies_repository.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:movies_app/movies/domain/entities/movie_details.dart';
+// import 'package:movies_app/movies/domain/entities/movie_details.dart';
 import '../../../core/error/failure.dart';
 import '../entities/movie.dart';
 
@@ -7,5 +7,5 @@ abstract class BaseMoviesRepository{
   Future<Either<Failure,List<Movies>>> getNowPlayingMovies();
   Future<Either<Failure,List<Movies>>> getPopularMovies();
   Future<Either<Failure,List<Movies>>> getTopRelatedMovies();
-  Future<Either<Failure,MovieDetails>> getMovieDetails();
+  // Future<Either<Failure,MovieDetails>> getMovieDetails();
 }
